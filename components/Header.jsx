219db70047685a0187cc59466455064b732776bd
Mariam <mariam.elbahey.hammam@gmail.com>
2025-03-15 23:51:45 +0200

    Your commit message

---
+++ components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+<<<<<<< HEAD
+    <header
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+=======
+    <header>
+>>>>>>> 9dadeac (new commit)
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+export default Header;
