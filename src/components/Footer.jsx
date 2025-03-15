bf449b07f2b2c5c53f9d20926dc97a739a714c5b
Mariam <mariam.elbahey.hammam@gmail.com>
2025-03-15 08:53:17 +0200

    Added inline styles to components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
