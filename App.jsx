bb64e84266c29a67729708e79c7f9cd61f673296
Mariam <mariam.elbahey.hammam@gmail.com>
2025-03-16 00:08:51 +0200

    my commit

--- App.jsx
+++ App.jsx
@@ -1,14 +1,15 @@
-// src/App.jsx
 import React from "react";
 import UserContext from "./UserContext";
 import ProfilePage from "./components/ProfilePage";
+
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
+      {" "}
+      {/* توفير البيانات */}
       <ProfilePage />
-      <UserContext />
     </UserContext.Provider>
   );
 }
