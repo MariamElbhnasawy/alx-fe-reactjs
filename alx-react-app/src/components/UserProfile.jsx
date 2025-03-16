21e5df134ea0f6fed7b2b63d0eb45796cd7cc6e4
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-03-16 23:51:01 +0200

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function UserProfile({ name, age, Bio }) {
+function UserProfile({ name, age, bio }) {
   return (
     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
       <h2 style={{ color: "blue" }}>{name}</h2>
       <p style={{ fontWeight: "bold", fontSize: "16px", margin: "10px 0" }}>
-        {"{props.name}", "{props.age}", "{props.Bio}"}
+        {"{props.name}", "{props.age}", "{props.bio}"}
       </p>
       <p style={{ fontSize: "14px", margin: "10px 0" }}>Age: {age}</p>
       <p style={{ fontSize: "14px", margin: "10px 0" }}>{bio}</p>
@@ -14,3 +14,4 @@ function UserProfile({ name, age, Bio }) {
 }
 
 export default UserProfile;
+/*Bio*/
