4450bf02bd0cf81461fcab5f3125f797941a59ca
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-03-16 23:45:36 +0200

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -5,8 +5,9 @@ function UserProfile({ name, age, bio }) {
     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
       <h2 style={{ color: "blue" }}>{name}</h2>
       <p style={{ fontWeight: "bold", fontSize: "16px", margin: "10px 0" }}>
-        Age: {age}
+        "{name}", "{age}", "{bio}", "Bio"
       </p>
+      <p style={{ fontSize: "14px", margin: "10px 0" }}>Age: {age}</p>
       <p style={{ fontSize: "14px", margin: "10px 0" }}>{bio}</p>
     </div>
   );
