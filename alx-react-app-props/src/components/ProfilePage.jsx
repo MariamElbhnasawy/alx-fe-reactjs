3edb75e01640a83bf24a4d5ce3d6444892c73f5c
Mariam <mariam.elbahey.hammam@gmail.com>
2025-03-16 06:46:53 +0200

    new commit

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function ProfilePage() {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Hello : {user.name} 👋</h2>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function UserProfile({ name, age, bio }) {
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{name}</h2>
+      <p style={{ fontWeight: "bold", fontSize: "16px", margin: "10px 0" }}>
+        Age: {age}
+      </p>
+      <p style={{ fontSize: "14px", margin: "10px 0" }}>{bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
