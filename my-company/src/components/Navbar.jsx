010a9a2891af8d0bc77fdcbf2a7eb03103cc5dc0
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-03-16 23:34:33 +0200

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "blue", display: "flex", justifyContent: "space-between" }}>
-      <h1>My Navbar</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    <nav style={{ padding: "10px", background: "#ddd" }}>
+      <Link to="/" style={{ margin: "0 10px" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 10px" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "0 10px" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "0 10px" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
