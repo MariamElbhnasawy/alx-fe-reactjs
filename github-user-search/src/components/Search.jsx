13017d68d7ea3cdc7bc0e804eb268a75403f63c4
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-04-12 08:13:40 +0200

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -44,7 +44,7 @@ const Search = () => {
         onChange={(e) => setRepos(e.target.value)}
       />
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user "Looks like we cant find the user"😢</p>}
+      {error && <p>Looks like we can't find the user ["map"] "Looks like we cant find the user"😢</p>}
       {user && (
         <div>
           <img src={user.avatar_url} alt={user.login} width="100" />
