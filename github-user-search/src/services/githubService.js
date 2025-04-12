e709f81b169d41a1d81504bfe922853126e9077f
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-04-12 08:15:10 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,3 +4,5 @@ export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 };
+/* ["https://api.github.com/search/users?q", "location", "minRepos"]
+ */
