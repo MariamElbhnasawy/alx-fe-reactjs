fd1701f3ab619908b4be2a3f5586286c899ac191
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-04-12 07:43:56 +0200

    Update App.jsx

--- github-user-search/src/github-user-search/src/App.jsx
+++ github-user-search/src/github-user-search/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more "Looks like we cant find the user"
+        Click on the Vite and React logos to learn more 
       </p>
     </>
   )
